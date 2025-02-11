c7a2831499be31fe851b33001f4744ba4cf86d58
Srisanjaykumar M <srisanjaykumar2000@gmail.com>
2025-02-11 13:50:37 +0530

    Fixed top bar issues

--- app/src/main/java/com/example/a30daysofrecipes/MainActivity.kt
+++ app/src/main/java/com/example/a30daysofrecipes/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a30daysofrecipes.model.RecipesRepository
 import com.example.a30daysofrecipes.ui.theme.DishList
 import com.example.a30daysofrecipes.ui.theme.DishTopAppBar
